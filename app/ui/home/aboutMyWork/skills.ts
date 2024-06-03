2a19a27a28c0e7a14b6ce783b1e4911d7188bdbe
Thalisu <thalisondmcc@gmail.com>
2024-06-03 16:29:26 -0300

    refactor: change skills

--- app/ui/home/aboutMyWork/skills.ts
+++ app/ui/home/aboutMyWork/skills.ts
@@ -1,18 +1,18 @@
 const skills = [
   {
-    label: "No Front-end:",
+    label: "Front-end:",
     description:
       "Desenvolver a sua interface de maneira unica, customizável, performática e escalável, com foco em uma experiência memorável para o cliente.",
-    bulletPoints: ["Animações suaves", "Performance", "Tecnologias modernas"],
+    bulletPoints: ["Typescript", "React", "Next"],
   },
   {
-    label: "No Back-end:",
+    label: "Back-end:",
     description:
-      "Desenvolver APIs REST e RESTful para integração com banco de dados noSQL ou SQL.",
+      "Desenvolver APIs REST e RESTful para interação com bancos de dados noSQL ou SQL.",
     bulletPoints: ["MongoDB", "PostgreSQL", "GraphQl"],
   },
   {
-    label: "No CI / CD",
+    label: "CI / CD:",
     description:
       "Desenvolver pipelines para reforçar se o app está realmente seguindo todas as boas praticas da empresa, se as novas features não afetam as features anteriores ..., e finalmente, se o app pode ir para a produção sem erros.",
     bulletPoints: ["GitHub Actions", "GitLab", "Jenkins"],
