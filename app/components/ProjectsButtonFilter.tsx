570784c8cafa77f446b24f9bb984903501127c11
Thalisu <thalisondmcc@gmail.com>
2024-06-04 14:25:10 -0300

    feat: add clickableContainer

--- app/components/ProjectsButtonFilter.tsx
+++ app/components/ProjectsButtonFilter.tsx
@@ -1,5 +1,6 @@
 import { ProjectsButtonFilter as PropTypes } from "../lib/types";
 import { poppins } from "../ui/fonts";
+import ClickableContainer from "./ClickableContainer";
 
 export default function ProjectsButtonFilter(props: PropTypes) {
   const handleMouseEnter = (i: number) => {
@@ -20,21 +21,22 @@ export default function ProjectsButtonFilter(props: PropTypes) {
   };
 
   return (
-    <button
+    <ClickableContainer
       className="relative h-10 w-1/5 cursor-none overflow-clip rounded border border-primary bg-primary px-4 py-2"
-      onMouseEnter={() => handleMouseEnter(props.i)}
-      onMouseLeave={() => handleMouseLeave(props.i)}
+      href=""
     >
       <div
-        className="absolute -bottom-10 left-0 right-0 h-full bg-secondary"
+        className="pointer-events-none absolute -bottom-10 left-0 right-0 h-full bg-secondary"
         id={`buttonCover${props.i}`}
       ></div>
       <span
         className={`${poppins.className} absolute bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center font-semibold text-secondary mix-blend-difference`}
         id={`buttonText${props.i}`}
+        onMouseEnter={() => handleMouseEnter(props.i)}
+        onMouseLeave={() => handleMouseLeave(props.i)}
       >
         {props.text}
       </span>
-    </button>
+    </ClickableContainer>
   );
 }
