1511455786b088736f00371dd436bf62a270ded7
Thalisu <thalisondmcc@gmail.com>
2024-06-04 00:16:33 -0300

    refactor: change folder

---
+++ app/lib/projects.ts
@@ -0,0 +1,20 @@
+import { Project } from "@/app/lib/types";
+
+const projects: Project[] = [
+  {
+    name: "Movie Streaming App",
+    type: "FullStack",
+    age: 2024,
+    img: "/imgs/movieStreamingHome.png",
+    href: "",
+  },
+  {
+    name: "Portfólio",
+    type: "FrontEnd",
+    age: 2024,
+    img: "/imgs/movieStreamingHome.png",
+    href: "",
+  },
+];
+
+export default projects;
