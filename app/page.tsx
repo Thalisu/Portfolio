17f32ac9936921dba985f7de733f8db6b603f3a2
Thalisu <thalisondmcc@gmail.com>
2024-06-07 08:59:29 -0300

    refactor: change padding and rename component

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import HeroSection from "./ui/home/heroSection";
 import AboutMyWork from "./ui/home/aboutMyWork";
 import Works from "./ui/home/works";
 import Contact from "./ui/home/contact";
-import RelativeToSelfContainer from "./components/CardAnimatedContainer";
+import CardAnimatedContainer from "./components/CardAnimatedContainer";
 
 export default function Home() {
   return (
     <main className="relative">
       <HeroSection />
-      <RelativeToSelfContainer className="boxShadow relative z-10 flex h-fit w-full flex-col items-center gap-28 rounded-[2rem] bg-secondary pb-96 pt-[5%]">
+      <CardAnimatedContainer className="boxShadow relative z-10 flex h-fit w-full flex-col items-center gap-28 rounded-[2rem] bg-secondary pb-[50svh] pt-[5%]">
         <AboutMyWork />
         <Works />
-      </RelativeToSelfContainer>
+      </CardAnimatedContainer>
       <Contact />
     </main>
   );
