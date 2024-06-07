f2df6c28e1aff86df5e0ece363d41976338f49e6
Thalisu <thalisondmcc@gmail.com>
2024-06-07 18:45:50 -0300

    fix: fix banner flicking in some screen widths

--- app/provider/routeTransition/Banners.tsx
+++ app/provider/routeTransition/Banners.tsx
@@ -10,32 +10,32 @@ export default forwardRef(function Banners(
     <div ref={ref}>
       <div
         id="banner-1"
-        className="fixed bottom-0 left-0 top-0 z-50 min-h-svh w-1/3 bg-neutral-950"
-      ></div>
-      <div
-        id="banner-2"
-        className="fixed bottom-0 left-2/3 top-0 z-50 min-h-svh w-1/3 bg-neutral-950"
+        className="fixed left-0 top-0 z-50 min-h-svh w-[33svw] bg-neutral-950"
       ></div>
       <div
         id="banner-3"
-        className="fixed bottom-0 left-1/3 top-0 z-50 flex min-h-svh w-[35%] items-center justify-center bg-neutral-950"
+        className="fixed left-[33svw] top-0 z-50 flex min-h-svh w-[35svw] items-center justify-center bg-neutral-950"
       >
         <p
-          className={`${poppins.className} text-5xl font-bold uppercase text-secondary`}
+          className={`${poppins.className} w-fit text-5xl font-bold uppercase text-secondary`}
           id="text"
         >
           {text}
         </p>
         <div
           id="loading"
-          className="animate-loading absolute bottom-1/3 inline-block aspect-square w-8 rounded border-4 border-[#e8e8e3]"
+          className="absolute bottom-1/3 inline-block aspect-square w-8 animate-loading rounded border-4 border-[#e8e8e3]"
         >
           <div
             id="loading"
-            className="animate-loadingInner w-full bg-primary align-top"
+            className="w-full animate-loadingInner bg-primary align-top"
           ></div>
         </div>
       </div>
+      <div
+        id="banner-2"
+        className="fixed left-[67svw] top-0 z-50 min-h-svh w-[33svw] bg-neutral-950"
+      ></div>
     </div>
   );
 });
