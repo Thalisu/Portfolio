6881641615dd822fb270bee5677b2c2b8c7b4fbf
Thalisu <thalisondmcc@gmail.com>
2025-01-07 16:51:57 -0300

    fix: fix horizontal screen

--- app/ui/home/heroSection/index.tsx
+++ app/ui/home/heroSection/index.tsx
@@ -11,20 +11,20 @@ export default function HeroSection() {
 
   return (
     <div
-      className="relative flex h-svh w-full flex-col items-center justify-center"
+      className="relative flex h-svh w-full flex-col items-center justify-between"
       ref={ref}
     >
-      <div className="relative flex h-fit w-fit flex-col gap-12 opacity-0">
+      <div className="relative flex h-fit w-fit grow flex-col justify-center gap-12 opacity-0">
         <TopText />
         <MiddleText />
         <BottomText />
       </div>
-      <div className="absolute bottom-0 left-0 right-0 h-1/6">
-        <div>
+      <div className="relative bottom-0 left-0 right-0 mb-2 mt-auto h-32 w-full">
+        <div className="">
           <ArrowDown className="absolute bottom-0 left-0 right-0 top-0 m-auto flex min-h-3 w-3 text-primary opacity-0" />
           <ArrowDown className="absolute bottom-0 left-0 right-0 top-0 m-auto min-h-3 w-3 text-primary opacity-0" />
         </div>
-        <CircularScrollTexts className="absolute bottom-0 left-0 right-0 top-0 m-auto w-32 text-primary opacity-0" />
+        <CircularScrollTexts className="m-auto w-32 text-primary opacity-0" />
       </div>
     </div>
   );
