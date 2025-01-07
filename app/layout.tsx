98f1d31de7eed97555e033bf83422967e98356d8
Thalisu <thalisondmcc@gmail.com>
2025-01-07 14:46:23 -0300

    fix: fix mobile overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,17 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`flex h-fit w-svw cursor-none flex-col items-center overflow-x-hidden bg-primary text-[#aeae9d] before:pointer-events-none before:fixed before:z-[1000000] before:h-full before:w-full before:opacity-20 before:[filter:url(#noiseFilter)]`}
+        className={`min-h-screen w-svw bg-primary text-[#aeae9d] before:pointer-events-none before:fixed before:z-[1000000] before:h-full before:w-full before:opacity-20 before:[filter:url(#noiseFilter)]`}
       >
-        <SmoothScroll>
-          <NoiseFilter className="pointer-events-none fixed h-0 w-0" />
-          <CursorProvider>
-            {children}
-          </CursorProvider>
-        </SmoothScroll>
+        <div
+          className="h-full w-full cursor-none flex-col items-center overflow-clip"
+          id="content-wrapper"
+        >
+          <SmoothScroll>
+            <NoiseFilter className="pointer-events-none fixed h-0 w-0" />
+            <CursorProvider>{children}</CursorProvider>
+          </SmoothScroll>
+        </div>
       </body>
     </html>
   );
